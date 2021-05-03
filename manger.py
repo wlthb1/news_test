97a635512c4b18996663c11e0acb9085e9a40211
wlthb1 <vcg92314917280@163.com>
2021-05-03 21:23:38 +0800

    项目的配置&数据库配置

--- manger.py
+++ manger.py
@@ -1,6 +1,22 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+
+class Config(object):
+    """ 项目的配置 """
+    DEBUG = True
+
+    # 配置数据库
+    SQLALCHEMY_DATABASE_URI = "mysql://root:mysql@127.0.0.1:3306/information_test"
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 app = Flask(__name__)
+
+# 加载配置
+app.config.from_object(Config)
+
+# 初始化数据库
+db = SQLAlchemy(app)
 
 @app.route("/")
 def index():
