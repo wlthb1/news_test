fb66912c267eae62ef7a9b1a923ccd89ba8768f0
wlthb1 <vcg92314917280@163.com>
2021-05-03 22:06:33 +0800

    抽取APP创建逻辑到业务模块中

--- manger.py
+++ manger.py
@@ -1,28 +1,6 @@
-from flask_wtf import CSRFProtect
-from redis import StrictRedis
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-from flask_session import Session
 from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
-from config import Config
-
-
-app = Flask(__name__)
-
-# 加载配置
-app.config.from_object(Config)
-
-# 初始化数据库
-db = SQLAlchemy(app)
-
-redis_store = StrictRedis(host=Config.REDIS_HOST, port=Config.REDIS_PORT)
-
-# 开启当前项目CSRF保护
-CSRFProtect(app)
-
-# 设置session指定保存位置
-Session(app)
+from info import app, db
 
 manager = Manager(app)
 
